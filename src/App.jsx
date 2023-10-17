2c7067e98bfb842774ce0b93befd74a94d6da47d
anhb8 <bichtram1307@gmail.com>
2023-10-17 03:00:11 -0500

    Updated navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import Header from './components/header/Header'
+import NavBar from './components/navbar/NavBar'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 const App = () => {
     return (
         <>
             <Header />
+            <NavBar />
+            <About />
+            <Experience />
+            <Skills />
+            <Portfolio />
+            <Contact />
+            <Footer />
+
         </>
     )
 }
