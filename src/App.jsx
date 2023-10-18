62d4ddbda3ed8b223d9544c97fe4b34788e8eca8
anhb8 <bichtram1307@gmail.com>
2023-10-18 02:50:06 -0500

    Created banner for website header

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import Experience from './components/experience/Experience';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+import Banner from './components/banner/Banner';
 
 const App = () => {
     return (
         <div className="App">
             <Header />
             <NavBar />
+            <Banner />
             <About />
             <Experience />
             <Skills />
