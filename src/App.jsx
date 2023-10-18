913591ea6f6c69a4b3a2b0616b465173a11b8853
anhb8 <bichtram1307@gmail.com>
2023-10-18 01:21:06 -0500

    Update assets folder and default CSS in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import Header from './components/header/Header'
-import NavBar from './components/navbar/NavBar'
-import About from './components/about/About'
-import Skills from './components/skills/Skills'
-import Experience from './components/experience/Experience'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import './App.css';
+import logo from './assets/img/logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/Header';
+import NavBar from './components/navbar/NavBar';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Experience from './components/experience/Experience';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
 const App = () => {
     return (
-        <>
+        <div className="App">
             <Header />
             <NavBar />
             <About />
@@ -19,8 +23,8 @@ const App = () => {
             <Contact />
             <Footer />
 
-        </>
+        </div>
     )
 }
 
-export default App 
+export default App;
